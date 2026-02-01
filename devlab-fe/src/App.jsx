0f32c5336b79e3b8fe02d50f5b28c848bad206da
id3ntity99 <jas06113@gmail.com>
2026-02-01 16:04:50 +0900

    First commit

---
+++ devlab-fe/src/App.jsx
@@ -0,0 +1,13 @@
+import { Router } from 'react-router-dom'
+import './App.css'
+import './components/Navigation'
+import NavigationBar from './components/Navigation'
+
+function App() {
+
+  return (
+    <NavigationBar/>
+  )
+}
+
+export default App
---
+++ devlab-fe/src/components/Navigation.jsx
@@ -0,0 +1,61 @@
+import { useState } from "react";
+import { useLocation } from "react-router-dom";
+import "./Navigation.css";
+
+export default function NavigationBar() {
+  const currentPath = useLocation().pathname;
+
+  const [activeLink, setActiveLink] = useState(currentPath);
+
+  const handleLinkClick = (href) => {
+    setActiveLink(href);
+  };
+
+  return (
+    <>
+      <nav>
+        <div className="nav-container">
+          <div className="logo">DevLab</div>
+          <div className="nav-center">
+            <a
+              href="/explore"
+              className={activeLink === "/explore" ? "active" : ""}
+              onClick={() => handleLinkClick("/explore")}
+            >
+              키트 둘러보기
+            </a>
+            <a
+              href="/create"
+              className={activeLink === "/create" ? "active" : ""}
+              onClick={() => handleLinkClick("/create")}
+            >
+              키트 만들기
+            </a>
+            <a
+              href="/community"
+              className={activeLink === "/community" ? "active" : ""}
+              onClick={() => handleLinkClick("/community")}
+            >
+              커뮤니티
+            </a>
+            <a
+              href="/pricing"
+              className={activeLink === "/pricing" ? "active" : ""}
+              onClick={() => handleLinkClick("/pricing")}
+            >
+              요금제
+            </a>
+          </div>
+          <div className="nav-right">
+            <a href="/login" className="btn btn-next">
+              로그인
+            </a>
+            <a href="/signup" className="btn btn-primary">
+              시작하기
+            </a>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+}
